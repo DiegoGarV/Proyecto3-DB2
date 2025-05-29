aaeac755995727a139c9fe02c26a68e077e01bf6
Czar272 <pablo2003cesar@gmail.com>
2025-05-29 16:25:54 -0600

    gitignore y nuestro rompecabezas

--- data/puzzle_data.py
+++ data/puzzle_data.py
@@ -62,5 +62,62 @@ rompecabezas_data = [
                 "entrada": "abajo"
             },
         ],
-    }
+    },
+    {
+        "nombre": "Aviones Puzzle",
+        "forma": "Rectangular",
+        "instrucciones": "Empieza por la oreja derecha",
+        "piezas": [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3},
+            {"id": 4},
+            {"id": 5},
+            {"id": 6},
+            {"id": 7},
+            {"id": 8},
+            {"id": 9},
+            {"id": 10},
+            {"id": 11},
+            {"id": 12},
+            {"id": 13},
+            {"id": 14},
+            {"id": 15},
+            {"id": 16},
+            {"id": 17},
+            {"id": 18},
+            {"id": 19},
+            {"id": 20},
+            {"id": 21},
+            {"id": 22},
+            {"id": 23},
+            {"id": 24},
+        ],
+        "conexiones": [
+            {
+                "desde": 1,
+                "hacia": 2,
+                "direccion": "Este",
+                "desde_tipo": "macho",
+                "hacia_tipo": "hembra",
+                "entrada": "derecha"
+            },
+            {
+                "desde": 2,
+                "hacia": 3,
+                "direccion": "Este",
+                "desde_tipo": "hembra",
+                "hacia_tipo": "macho",
+                "entrada": "derecha"
+            },
+            {
+                "desde": 2,
+                "hacia": 3,
+                "direccion": "Oeste",
+                "desde_tipo": "hembra",
+                "hacia_tipo": "macho",
+                "entrada": "derecha"
+            },
+        ],
+    },
 ]
