d94be52513e450515ff4e242df496de00e636452
DiegoGarV <dgv3105@gmail.com>
2025-05-29 04:45:22 -0600

    feat: safe connection

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-NEO4J_URI="neo4j+s://912b287e.databases.neo4j.io"
-NEO4J_USERNAME="neo4j"
-NEO4J_PASSWORD="b8d8GP0zdVxbJtjyBFE2eDGR9TtKKnYrpiQa-q8Wizg"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
